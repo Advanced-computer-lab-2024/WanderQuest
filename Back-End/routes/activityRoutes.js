ca0bef83557189feab9116cb4308ca35a7af3f41
fareedasaad2710 <saadfarida123@gmail.com>
2024-10-04 23:31:16 +0300

    modifications on the activity,itinerary

--- Back-End/routes/activityRoutes.js
+++ Back-End/routes/activityRoutes.js
@@ -5,7 +5,8 @@ const {
      createActivity,
      readActivities,
      updateActivity,
-     deleteActivity } 
+     deleteActivity,
+     readOneActivity } 
 = require('../controllers/advertiserController');
 
 const router = express.Router()
@@ -15,5 +16,5 @@ router.post('/activity',createActivity);
 router.get('/activities',readActivities);
 router.put('/activity/:id',updateActivity);
 router.delete('/activity/:id',deleteActivity);
-
+router.get('/activity/:id',readOneActivity)
 module.exports = router
