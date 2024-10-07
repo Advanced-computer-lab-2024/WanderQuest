15f7125798398e1b75fa67ab95fc747c1059de86
Adham <adhamhabdelaziz@gmail.com>
2024-10-07 19:14:44 +0300

    admin page almost ready

--- Front-End/wanderquest/src/app/admin/page.js
+++ Front-End/wanderquest/src/app/admin/page.js
@@ -5,8 +5,6 @@ const AdminPage = () => {
     return (
         <div>
             <Navbar/>
-            <Link href="/admin/acts"><button>Activities</button></Link>
-        <Link href="/admin/pref"><button>Preference Tags</button></Link>
             <div>
                 <Admin />
             </div>
