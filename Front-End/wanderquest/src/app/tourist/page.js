651fecfbbf10761c7a36135a836cd9fd637463cf
abdsh55 <abdullahsherif73+git@gmail.com>
2024-10-07 22:06:51 +0300

    Add Display Itineraries in tourist

--- Front-End/wanderquest/src/app/tourist/page.js
+++ Front-End/wanderquest/src/app/tourist/page.js
@@ -1,11 +1,20 @@
-import Navbar from "../../../components/Navbar";
+'use client';
+import { useRouter } from 'next/navigation';
+import Navbar from '../../../components/Navbar';
 
 export default function Tourist() {
+    const router = useRouter();
+
+    const handleRedirect = () => {
+        router.push('/tourist/iti');
+    };
+
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <h1>Tourist Page</h1>
             <p>Welcome to the Tourist page!</p>
+            <button onClick={handleRedirect}>Go to Itinerary</button>
         </div>
     );
 };
