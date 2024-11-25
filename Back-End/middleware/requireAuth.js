c6be050f97d30ee19a68d47934b2abce640c963f
abdsh55 <abdullahsherif73+git@gmail.com>
2024-11-26 01:17:27 +0200

    Fix requireAuth

--- Back-End/middleware/requireAuth.js
+++ Back-End/middleware/requireAuth.js
@@ -5,23 +5,26 @@ const requireAuth = (config = {}) => {
     return async (req, res, next) => {
         const token = req.cookies.jwt;
 
+
         if (!token) {
             return res.status(401).json({ error: 'You are not logged in' });
         }
 
+        jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
+            const extractedID = decodedToken._id._id;
 
-        const { _id } = jwt.verify(token, process.env.SECRET);
-        jwt.verify(token, process.env.SECRET, async (err, _id) => {
             if (err) {
                 res.status(401).json({ message: "You are not logged in." })
             } else {
-                console.log('Token verified, user ID:', _id);
+                console.log('Token verified, user ID:', extractedID);
 
                 // Select both _id and role from the database
-                req.user = await User.findById(_id).select('_id role');
+                req.user = await User.findById(extractedID).select('_id role');
                 if (!req.user) {
                     return res.status(401).json({ error: 'User not found' });
                 }
+
+                console.log('User:', req.user);
 
                 // Check if the user has the required role
                 if (config.role && req.user.role !== config.role) {
@@ -33,4 +36,4 @@ const requireAuth = (config = {}) => {
     };
 };
 
-module.exports = { requireAuth };
+module.exports = requireAuth;
--- Back-End/routes/touristRoutes.js
+++ Back-End/routes/touristRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+
 // controller functions
 const { getAllPlaces, getPlaceById } = require('../controllers/tourGovernerController')
 const { getAvailableProducts } = require('../controllers/adminController')
@@ -22,15 +23,15 @@ const {
     getTotalPoints,
     getAllCurrencies
 } = require('../controllers/touristController');
-// const requireAuth = require('../middleware/requireAuth');
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
 
 // require auth for all tourist routes
-// router.use(requireAuth);
+router.use(requireAuth({ role: 'tourist' }));
 
 // routes
-router.get('/profile/:id', getProfile);
+router.get('/profile', getProfile);
 router.put('/profile/:id', updateProfile);
 router.get('/touristId', getTouristId);
 router.get('/availableProducts', getAvailableProducts);
@@ -48,16 +49,16 @@ router.get('/upcomingPlaces', getAllPlaces);
 
 // Sharing activity via link or email
 router.get('/upcomingPlaces/:id', getPlaceById);
-router.post('/rateProduct/:id',rateProduct);
-router.post('/reviewProduct/:id', reviewProduct); 
+router.post('/rateProduct/:id', rateProduct);
+router.post('/reviewProduct/:id', reviewProduct);
 router.get('/currencies', getAllCurrencies);
 router.patch('/changePreferredCurrency/:id', changePreferredCurrency);
-router.patch('/redeem/:id',redeemPoints);
-router.post('/fileComplaint',fileComplaint);
-router.get('/myComplaints/:id',myComplaints);
-router.get('/level/:id',getLevel)
-router.get('/availablePoints/:id',getavailablePoints)
-router.get('/totalPoints/:id',getTotalPoints)
+router.patch('/redeem/:id', redeemPoints);
+router.post('/fileComplaint', fileComplaint);
+router.get('/myComplaints/:id', myComplaints);
+router.get('/level/:id', getLevel)
+router.get('/availablePoints/:id', getavailablePoints)
+router.get('/totalPoints/:id', getTotalPoints)
 
 
 module.exports = router;
