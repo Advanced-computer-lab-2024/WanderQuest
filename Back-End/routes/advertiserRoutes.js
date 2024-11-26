8a421a11137da8799ca3e749c7702fce0e7b3b75
AhmedWaleedElFar <ahmedw.delfar2002@gmail.com>
2024-11-26 03:30:00 +0200

    Fix: the first half of the authentication links

--- Back-End/routes/advertiserRoutes.js
+++ Back-End/routes/advertiserRoutes.js
@@ -20,18 +20,18 @@ const router = express.Router();
 
 // get and update profile will use query param for now untill we have a proper auth
 // routes
-router.get('/activity/:id', requireAuth({ role: 'advertiser' }), readOneActivity);
+router.get('/activity/:id', readOneActivity);
 router.get('/profile', requireAuth({ role: 'advertiser' }), getProfile);
 router.put('/profile', requireAuth({ role: 'advertiser' }), updateProfile);
 router.post('/uploadLogo', requireAuth({ role: 'advertiser' }), uploadLogo);
 router.get('/logo', requireAuth({ role: 'advertiser' }), getLogo);
 router.get('/allAdvertisers', requireAuth({ role: 'admin' }), getAllAdvertisers);
 router.post('/activity', requireAuth({ role: 'advertiser' }), createActivity);
-router.get('/activities', requireAuth({ role: 'advertiser' }), readActivities);
+router.get('/activities', readActivities);
 router.put('/activity/:id', requireAuth({ role: 'advertiser' }), updateActivity);
 router.delete('/activity/:id', requireAuth({ role: 'advertiser' }), deleteActivity);
 router.get('/myActivities', requireAuth({ role: 'advertiser' }), myCreatedActivities);
 router.post('/transportation/create', requireAuth({ role: 'advertiser' }), createTransportation);
-router.get('/transportations', requireAuth({ role: 'advertiser' }), getAllTransportations);
+router.get('/transportations', getAllTransportations);
 
 module.exports = router;
