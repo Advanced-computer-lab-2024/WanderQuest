a8a57249c4eb713d17b7be4d3e8f809a1620fcd0
abdsh55 <abdullahsherif73+git@gmail.com>
2024-11-27 13:35:55 +0200

    Create Login method

--- Back-End/routes/authenticationRoutes.js
+++ Back-End/routes/authenticationRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const requireAuth = require('../middleware/requireAuth');
 
 // controller functions
-const { registerUser, uploadDocuments, getUserDocuments, getUsersRequestingAcceptance, getDocumentByFileID, changePassword, acceptUser, acceptTerms, requestAccountDeletion } = require('../controllers/authenticationController');
+const { registerUser, login, uploadDocuments, getUserDocuments, getUsersRequestingAcceptance, getDocumentByFileID, changePassword, acceptUser, acceptTerms, requestAccountDeletion } = require('../controllers/authenticationController');
 
 const router = express.Router();
 
 // routes
 router.post('/register', registerUser);
+router.post('/login', login);
 router.post('/changePassword', requireAuth(), changePassword);
 router.post('/uploadDocuments', requireAuth(), uploadDocuments);
 router.get('/getDocuments', requireAuth(), getUserDocuments);
