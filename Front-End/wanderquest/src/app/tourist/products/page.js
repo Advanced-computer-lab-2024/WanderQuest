93d432172a5cbf9255b385d55a098f4487868e6b
Abdullah-Sherif <abdullahsherif73+git@gmail.com>
2024-12-09 01:56:11 +0200

    Make wishlist only load for tourist

--- Front-End/wanderquest/src/app/tourist/products/page.js
+++ Front-End/wanderquest/src/app/tourist/products/page.js
@@ -1,12 +1,12 @@
 'use client';
 import Products from '../../../../components/Products'; // Adjust the import path as needed
 import Navbar from '../../../../components/Navbar'; // Assuming you have a Navbar component
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 const museumpage = () => {
-    return (
-        <div>
-            <Navbar />
-            {/* <motion.p
+  return (
+    <div>
+      <Navbar />
+      {/* <motion.p
   style={{ border: '0px solid black' ,width:400}}
   animate={{ scale: 2.5,  color: '#ff2994', x: 900 }}
   transition={{duration:0.5}}
@@ -14,9 +14,9 @@ const museumpage = () => {
   Products Page
 </motion.p> */}
 
-           <Products role={"Tourist"}/>
-        </div>
-    );
+      <Products role={"Tourist"} />
+    </div>
+  );
 };
 
 export default museumpage;
